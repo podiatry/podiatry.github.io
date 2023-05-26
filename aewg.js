23a631400cf276a146afa9c64f046a8e9810872e
Jacob Heifetz-Licht <134099549+podiatry@users.noreply.github.com>
2023-05-25 20:53:27 -0400

    a
    a

--- aewg.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('navbar-toggle').addEventListener('click', function() {
-  var navbarMenu = document.getElementById('navbar-menu');
-  navbarMenu.classList.toggle('active');
-});
